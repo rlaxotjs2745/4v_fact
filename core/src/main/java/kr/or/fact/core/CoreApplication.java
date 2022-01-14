6bcbe5dec99793b70434047b64e01738c2165967
taeseon <taeseon1997@gmail.com>
2022-01-14 14:49:14 +0900

    add CoreApplication

---
+++ core/src/main/java/kr/or/fact/core/CoreApplication.java
@@ -0,0 +1,16 @@
+package kr.or.fact.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+public class CoreApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+}
