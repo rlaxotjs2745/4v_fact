3c3391b0ec7c3f88562d5d584780c193634ca7f4
taeseon <taeseon1997@gmail.com>
2022-01-14 17:02:41 +0900

    delete admin plugin version release

--- core/src/main/java/kr/or/fact/core/CoreApplication.java
+++ core/src/main/java/kr/or/fact/core/CoreApplication.java
@@ -2,11 +2,8 @@ package kr.or.fact.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
