4624e89193b9000c6fc61d8527eccfa268281842
lee-jung-bin <leebin@4thevision.com>
2022-05-02 13:12:34 +0900

    add deploy pom

--- api/src/main/java/kr/or/fact/api/ApiApplication.java
+++ api/src/main/java/kr/or/fact/api/ApiApplication.java
@@ -3,13 +3,17 @@ package kr.or.fact.api;
 import org.apache.logging.log4j.core.util.JsonUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class ApiApplication extends SpringBootServletInitializer {
-
+@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+    return application.sources(ApiApplication.class);
+}
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
