7175e1a3caea5700478fb8665aa30ce767a36425
WiNMasTeR-SW <sungwoong@4thevision.com>
2022-12-28 11:12:16 +0900

    홍보자료 관리 함수 관련된 파일 변경 : getPRContentList

--- web_mobile_gimje/src/main/java/kr/or/fact/web_mobile_gimje/controller/MobileIndexController.java
+++ web_mobile_gimje/src/main/java/kr/or/fact/web_mobile_gimje/controller/MobileIndexController.java
@@ -390,7 +390,10 @@ public class MobileIndexController {
             return "brd_announce_blank";
         }
         model.addAttribute("total_count",promotionCount);
-        List<PRContentVO> prContentList = prContentService.getPRContentList(page,list_amount);
+        ParamPageListFilteredVO paramVo = new ParamPageListFilteredVO();
+        paramVo.setPage_num(page);
+        paramVo.setAmount(list_amount);
+        List<PRContentVO> prContentList = prContentService.getPRContentList(paramVo);
         model.addAttribute("prContentList",prContentList);
 
         model.addAttribute("cur_page",page);
